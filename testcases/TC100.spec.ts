698bebb73a442ca02285b2b9c844b7a802b8c334
Robert <rgjoyce111@gmail.com>
2025-03-07 08:19:44 +0600

    added tc100

---
+++ testcases/TC100.spec.ts
@@ -0,0 +1,86 @@
+import { test, expect, ElementHandle } from '@playwright/test';
+import { runTC000, performLogin } from './TC000.spec'; // Adjust the import path as necessary
+import { CreatePartsDatabasePage } from '../pages/PartsDatabasePage';
+import { ENV, SELECTORS } from '../config'; // Import the configuration
+import logger from '../lib/logger';
+import { allure } from 'allure-playwright';
+
+import testData1 from '../testdata/PD18-T1.json'; // Import your test data
+import testData2 from '../testdata/PD18-T2.json'; // Import your test data
+import testData3 from '../testdata/PD18-T3.json'; // Import your test data
+
+
+const LEFT_DATA_TABLE = "BasePaginationTable-Table-Component-product";
+const LEFT_DATA_TABLE_URGENCY_DATA_COL = "ShipmentsListTable-TableRow-HeaderDateByUrgency";
+const LEFT_DATA_TABLE_PLANNED_DATA_COL = "ShipmentsListTable-TableRow-HeaderDateShipmentPlan";
+const LEFT_DATA_TABLE_SEARCHABLE_FIELD = "ShipmentsListTable-TableRow-HeaderOrder";
+
+const CENTER_DATA_TABLE = "BasePaginationTable-Table-Component-cbed";
+
+const RIGHT_DATA_TABLE = "BasePaginationTable-Table-Component-detal";
+const RIGHT_DATA_TABLE_URGENCY_DATA_COL = "AssemblySclad-PrintTableHeader-UrgencyDateColumn";
+const RIGHT_DATA_TABLE_PLANNED_DATA_COL = "AssemblySclad-PrintTableHeader-PlannedShipmentDateColumn";
+const RIGHT_DATA_TABLE_SUBPLANNED_DATA_COL = "AssemblySclad-PrintTableHeader-ShipmentsColumn";
+const RIGHT_DATA_TABLE_SEARCHABLE_COLS1 = "AssemblySclad-PrintTableHeader-TypeColumn";
+const RIGHT_DATA_TABLE_SEARCHABLE_COLS2 = "AssemblySclad-PrintTableHeader-DesignationColumn";
+const RIGHT_DATA_TABLE_SEARCHABLE_COLS3 = "AssemblySclad-PrintTableHeader-NameColumn";
+const RIGHT_DATA_TABLE_ORDERS_ICON_COL = "AssemblySclad-PrintTableHeader-ShipmentsColumn";
+
+const RIGHT_MODAL_WINDOW_ID = "ModalShipmentsToIzed-destroyModalRight";
+const RIGHT_MODAL_TABLE_ID = "ModalShipmentsToIzed-table-buyers";
+const RIGHT_MODAL_TABLE_COL3 = "ModalShipmentsToIzed-thead-th3-buyers";
+const RIGHT_MODAL_TABLE_COL4 = "ModalShipmentsToIzed-thead-th4-buyers";
+const RIGHT_DATA_TABLE_CELL_X = "AssemblySclad-PrintTableBody-";
+const LEFT_DATA_TABLE_CELL_X = "ShipmentsListTable-orderRow";
+// Страница: База деталей
+export const runTC100 = () => {
+    logger.info(`Starting test: Validating full specifications ( Полная спецификация ) for an item on parts database Page`);
+    ;
+
+    // Use a separate step to initialize the База деталей
+    test.beforeEach(async ({ page }) => {
+        const shortagePage = new CreatePartsDatabasePage(page);
+
+        await allure.step('Step 1: Open the login page and login', async () => {
+            await performLogin(page, '001', 'Перов Д.А.', '54321');
+        });
+
+        await allure.step('Step 2: Navigate to Склад', async () => {
+            await page.waitForTimeout(5000);
+            await shortagePage.goto(SELECTORS.MAINMENU.PARTS_DATABASE.URL);
+        });
+
+    });
+    test.skip('Test Case 0: База деталей Page - Scan tables within a specific element', async ({ page }) => {
+        test.setTimeout(600000);
+        allure.label('severity', 'normal');
+        allure.label('epic', 'База деталей');
+        allure.label('feature', 'База деталей');
+        allure.label('story', 'Verify table structures');
+        allure.description('Verify  База деталей Page - Scan tables within a specific element.');
+
+        await allure.step('Step 1: Validate the structure of the tables on the page', async () => {
+            const shortagePage = new CreatePartsDatabasePage(page);
+            await page.waitForLoadState('networkidle');
+            const result = await shortagePage.scanTablesWithinElement(page, 'App-RouterView'); // Replace with your data-testid
+            expect(result.success, 'Validation failed with the following errors:\n' + result.errors.join('\n')).toBeTruthy();
+        });
+    });
+    test('Test Case 1: База деталей Page - Select a product and Click the Edit button', async ({ page }) => {
+        test.setTimeout(600000);
+        allure.label('severity', 'normal');
+        allure.label('epic', 'База деталей');
+        allure.label('feature', 'База деталей');
+        allure.label('story', 'Edit Product');
+        allure.description('База деталей Page - Select first Product.');
+
+        await allure.step('Step 1: Select the first product and click edit', async () => {
+            const shortagePage = new CreatePartsDatabasePage(page);
+            await page.waitForLoadState('networkidle');
+            const table = await shortagePage.findTable('BasePaginationTable-Table-Component-product');
+
+
+        });
+    });
+
+};
