52b0784f26b73971bbdf561611c432d63aedfc77
Robert <rgjoyce111@gmail.com>
2025-05-23 16:19:38 +0600

    timing

--- testcases/U004.spec.ts
+++ testcases/U004.spec.ts
@@ -2062,6 +2062,7 @@ export const runU004 = () => {
                 ];
 
                 for (const item of itemsToRemove) {
+                    await page.waitForTimeout(500);
                     const shortagePage = new CreatePartsDatabasePage(page);
                     await shortagePage.removeItemFromSpecification(
                         page,
