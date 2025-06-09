da85c03a72f42731991cf535eba67217bb9591a9
Robert <rgjoyce111@gmail.com>
2025-06-09 15:26:07 +0600

    config update

--- config.ts
+++ config.ts
@@ -13,10 +13,10 @@
  */
 
 export const ENV = {
-    BASE_URL: process.env.BASE_URL || "http://localhost:8080/",
+    BASE_URL: process.env.BASE_URL || "http://test.pksep.ru/",
     HEADLESS: process.env.HEADLESS === "true",
     TIMEOUT: process.env.TIMEOUT ? parseInt(process.env.TIMEOUT) : 5000,
-    TEST_SUITE: 'TC100',
+    TEST_SUITE: 'U005',
     TEST_DIR: '.',
     DEBUG: false,
 };
